5b2eb3b43a54015a72bf29b1fcc66ca96899627e
NnirajanDahal <nirajandahal58@gmail.com>
2023-07-28 10:34:35 +0545

    day1Completed

---
+++ lib/day_1/widgetlifecycle/widgetlifecycle.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class NumberWidget extends StatefulWidget {
+  const NumberWidget({super.key});
+
+  @override
+  _NumberWidgetState createState() {
+    return _NumberWidgetState();
+  }
+}
+
+class _NumberWidgetState extends State<NumberWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant NumberWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
